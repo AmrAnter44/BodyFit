1aa52c1f808b9fce9da969613f6f3e679290b6ff
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-10-27 20:14:34 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,6 @@ import Home from './comp/Home';
 import Layout from './Layout';
 import Coaches from './comp/Coaches';
 import Classes from './comp/Classes';
-import Men from './comp/Map/Men';
-import Ladies from './comp/Map/Ladies';
-import Cardio from './comp/Map/Cardio';
-import Weight from './comp/Map/Weight';
-import Leg from './comp/Map/Leg';
-import Fitness from './comp/Map/Fitness';
-import Spa from './comp/Map/Spa';
-import Caples from './comp/Map/Cables';
-import Machines from './comp/Map/Machines';
-import Bar from './comp/Map/Bar';
-import Online from './comp/Online';
-import SignOnline from './comp/SignOnline';
-import Map from './comp/Map';
-import Gold from './comp/vip/Gold';
-import Shop from './comp/Shop';
 import { Analytics } from "@vercel/analytics/react"
 import AdminLogin from './comp/admin/AdminLogin';
 import AdminDashboard from './comp/admin/AdminDashboard';
@@ -32,21 +17,6 @@ function App() {
         { index: true, element: <Home /> },
         { path: "/coaches", element: <Coaches /> },
         { path: "/classes", element: <Classes /> },
-        { path: "/map", element: <Map /> },
-        { path: "/Men", element: <Men /> },
-        { path: "/Ladies", element: <Ladies /> },
-        { path: "/Cardio", element: <Cardio /> },
-        { path: "/Weight", element: <Weight /> },
-        { path: "/Leg", element: <Leg /> },
-        { path: "/Fitness", element: <Fitness /> },
-        { path: "/Spa", element: <Spa /> },
-        { path: "/Caples", element: <Caples /> },
-        { path: "/Machines", element: <Machines /> },
-        { path: "/Bar", element: <Bar /> },
-        { path: "/online", element: <Online /> },
-        { path: "/sign", element: <SignOnline /> },
-          { path: "/gold", element: <Gold/> },
-          { path: "/shop", element: <Shop/> },
                 { path: "/admin-login", element: <AdminLogin /> },
       { path: "/admin-dashboard", element: <AdminDashboard /> },
       ],
