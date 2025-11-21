64229c40d35891a0e3126874a250cd9d7f11529d
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-11-21 17:19:02 +0200

    asd

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const mockOffers = [
-  { id: 1, duration: '1 Month', price: '450', price_new: '300', private: '1',invite: '1' },
+  { id: 1, duration: '1 Month', price: '450', price_new: '350', private: '1',invite: '1' },
   { id: 2, duration: '3 Months', price: '1350', price_new: '700', private: '2',  invite: '2' },
   { id: 3, duration: '6 Months', price: '2400', price_new: '1200', private: '4',  invite: '4' },
   { id: 4, duration: '12 Months', price: '3600', price_new: '1800', private: '8',  invite: '8' },
